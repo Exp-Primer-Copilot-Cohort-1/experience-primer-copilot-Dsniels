0f3a616af37a9279ec36cd450489340a45b28e45
Daniel Salazar <121785859+Dsniels@users.noreply.github.com>
2024-03-26 05:51:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,71 +1,30 @@
 // Create web server
+// Create a web server that listens on port 3000 and serves the comments.js file. If the request URL is /comments.js, the server should respond with the contents of the comments.js file. Use the fs module to read the file and send it to the client.
 
+// Use the following code to read the file:
 
-// Define dependencies
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+// fs.readFile('comments.js', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   res.end(data);
+// });
+// This code reads the comments.js file and sends it to the client. The res object is the response object that is used to send data to the client. Use the res.end() method to send the data to the client.
 
-// Start server
-app.listen(3000, function() {
-  console.log('Server running on http://localhost:3000');
-});
+const http = require('http');
+const fs = require('fs');
 
-// Define routes
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-// Get comments from database
-app.get('/comments', function(req, res) {
-  MongoClient.connect('mongodb://localhost:27017/comments', function(err, db) {
-    assert.equal(null, err);
-    db.collection('comments').find().toArray(function(err, comments) {
-      assert.equal(null, err);
-      res.send(comments);
+const server = http.createServer((req, res) => {
+  if (req.url === '/comments.js') {
+    fs.readFile('comments.js', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
     });
-  });
+  }
 });
 
-// Post comments to database
-app.post('/comments', function(req, res) {
-  MongoClient.connect('mongodb://localhost:27017/comments', function(err, db) {
-    assert.equal(null, err);
-    db.collection('comments').insertOne(req.body, function(err, result) {
-      assert.equal(null, err);
-      res.send(result);
-    });
-  });
-});
-
-// Serve static files
-app.get('/style.css', function(req, res) {
-  res.sendFile(__dirname + '/public/style.css');
-});
-
-// Serve index.html
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-// Serve comments.js
-app.get('/comments.js', function(req, res) {
-  res.sendFile(__dirname + '/public/comments.js');
-});
-
-// Serve 404.html
-app.get('*', function(req, res) {
-  res.status(404).sendFile(__dirname + '/public/404.html');
-});
-
-// Serve 500.html
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).sendFile(__dirname + '/public/500.html');
-});
-
-// Serve 400.html
-app.use(function(req, res) {
-  res.status(400).sendFile(__dirname + '/public/400.html');
-});
+server.listen(3000);
